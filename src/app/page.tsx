3f04ddf7cf48b16e8cbe9ce996b3d7f28ea4adc4
Mohammad Azimi <mmaz13841384@gmail.com>
2025-07-21 16:24:35 +0330

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Header from "./components/header/Header";
 import LeftBar from "./components/leftBar/LeftBar";
 import Activities from "./components/activities/Activities";
